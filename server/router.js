a87b4cfe7cf2aa4429ac0c3cef381da3f8f3b027
linyouxun <linyouxun@xiao100.com>
2020-06-20 15:18:29 +0800

    fix: url update

--- server/router.js
+++ server/router.js
@@ -1,6 +1,10 @@
 var Router = require("koa-router");
 var router = new Router();
 
+router.get("/", (ctx, next) => {
+  ctx.body = "Hello world";
+});
+
 router.post("/git/config", async (ctx, next) => {
   console.log(ctx.request.body);
   ctx.body = ctx.request.body;
