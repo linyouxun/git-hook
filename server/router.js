e78eec676b7220f2d599fdc4ea3e727bf4b7ccce
linyouxun <linyouxun@xiao100.com>
2020-06-20 15:13:11 +0800

    fix: request nody bug

--- server/router.js
+++ server/router.js
@@ -2,8 +2,8 @@ var Router = require("koa-router");
 var router = new Router();
 
 router.post("/git/config", async (ctx, next) => {
-  console.log(ctx.body);
-  ctx.body = {};
+  console.log(ctx.request.body);
+  ctx.body = ctx.request.body;
 });
 
 module.exports = router;
